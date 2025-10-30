c7506f2afe5e64f96e40b5f0a6ee15749ed38f0c
Labaile <labaile1@gmail.com>
2025-10-29 20:45:06 -0400

    homepage loaded and working

--- components/PortfolioContent.tsx
+++ components/PortfolioContent.tsx
@@ -1,10 +1,21 @@
 import { HeroSection } from "@/components/sections/HeroSection";
+import { AboutSection } from "@/components/sections/AboutSection";
+import { TestimonialsSection } from "@/components/sections/TestimonialsSection";
+import { SkillsSection } from "@/components/sections/SkillsSection";
+import { ExperienceSection } from "@/components/sections/ExperienceSection";
+import { EducationSection } from "@/components/sections/EducationSection";
+import { ProjectsSection } from "@/components/sections/ProjectsSection";
+import { CertificationsSection } from "@/components/sections/CertificationsSection";
+import { AchievementsSection } from "@/components/sections/AchievementsSection";
+import { ServicesSection } from "@/components/sections/ServicesSection";
+import { BlogSection } from "@/components/sections/BlogSection";
+import { ContactSection } from "@/components/sections/ContactSection";
 
 async function PortfolioContent() {
   return (
     <>
-      <HeroSection />
-      {/* <AboutSection />
+    <HeroSection />
+    <AboutSection />
     <TestimonialsSection />
     <SkillsSection />
     <ExperienceSection />
@@ -14,7 +25,7 @@ async function PortfolioContent() {
     <AchievementsSection />
     <ServicesSection />
     <BlogSection />
-    <ContactSection /> */}
+    <ContactSection /> 
     </>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,16 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
         protocol: "https",
         hostname: "cdn.sanity.io",
+        port:'',
+        pathname: '/images/**'
       },
     ],
   },
